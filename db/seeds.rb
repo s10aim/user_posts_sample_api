3c7648568ac83a9db9d542093b52b6e56109df24
tanahara <s10.vermouth.4869@gmail.com>
2021-08-07 14:58:08 +0900

    postsの初期データ投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,7 @@
-User.find_or_create_by!(id: 1, name: 'aimi')
+user1 = User.find_or_create_by!(id: 1, name: 'aimi')
+user2 = User.find_or_create_by!(id: 2, name: 'ayane')
+
+5.times do |n|
+  user1.posts.create!(body: "#{user1.name}-post-#{n}")
+  user2.posts.create!(body: "#{user2.name}-post-#{n}")
+end
